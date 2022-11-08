9117398ca19247964a4d3994703b28068d1d132d
coolbyte <ilovelovelove366@gmail.com>
2022-11-08 16:16:05 +0800

    fix reload on change https://github.com/devattic/ConfigCrypter/pull/6

--- ConfigCrypter/ConfigProviders/Json/EncryptedJsonConfigProvider.cs
+++ ConfigCrypter/ConfigProviders/Json/EncryptedJsonConfigProvider.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.Configuration.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using DevAttic.ConfigCrypter.Crypters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace DevAttic.ConfigCrypter.ConfigProviders.Json
 {
@@ -19,22 +26,140 @@ namespace DevAttic.ConfigCrypter.ConfigProviders.Json
         }
 
         /// <summary>
-        /// Loads the JSON configuration file and decrypts all configured keys with the given crypter.
+        /// Loads the JSON configuration from stream and decrypts all configured keys with the given crypter.
         /// </summary>
-        public override void Load()
+        public override void Load(Stream stream)
         {
-            base.Load();
-
-            using (var crypter = _jsonConfigSource.CrypterFactory(_jsonConfigSource))
+            try
             {
-                foreach (var key in _jsonConfigSource.KeysToDecrypt)
+                using (var crypter = _jsonConfigSource.CrypterFactory(_jsonConfigSource))
                 {
-                    if (Data.TryGetValue(key, out var encryptedValue))
-                    {
-                        Data[key] = crypter.DecryptString(encryptedValue);
-                    }
+                    Data = EncryptedJsonConfigurationFileParser.Parse(stream, crypter, _jsonConfigSource.KeysToDecrypt);
                 }
             }
+            catch (JsonException e)
+            {
+                throw new FormatException($"Error JSONParseError {e.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Unable to decrypt keys", ex);
+            }
+        }
+    }
+
+
+    internal class EncryptedJsonConfigurationFileParser
+    {
+        private EncryptedJsonConfigurationFileParser()
+        {
+        }
+
+        private readonly IDictionary<string, string> _data =
+            new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        private readonly Stack<string> _context = new Stack<string>();
+        private string _currentPath;
+        private ICrypter _crypter;
+        private List<string> _keysToDecrypt;
+
+        public static IDictionary<string, string> Parse(Stream input, ICrypter crypter, List<string> keysToDecrypt)
+            => new EncryptedJsonConfigurationFileParser().ParseStream(input, crypter, keysToDecrypt);
+
+        private IDictionary<string, string> ParseStream(Stream input, ICrypter crypter, List<string> keysToDecrypt)
+        {
+            _data.Clear();
+
+            var jsonDocumentOptions = new JsonDocumentOptions
+            {
+                CommentHandling = JsonCommentHandling.Skip,
+                AllowTrailingCommas = true,
+            };
+
+            using (var reader = new StreamReader(input))
+            using (var doc = JsonDocument.Parse(reader.ReadToEnd(), jsonDocumentOptions))
+            {
+                if (doc.RootElement.ValueKind != JsonValueKind.Object)
+                {
+                    throw new FormatException($"Error UnsupportedJSONToken {doc.RootElement.ValueKind}");
+                }
+
+                _crypter = crypter;
+                _keysToDecrypt = keysToDecrypt;
+                VisitElement(doc.RootElement);
+            }
+
+            return _data;
+        }
+
+        private void VisitElement(JsonElement element)
+        {
+            foreach (var property in element.EnumerateObject())
+            {
+                EnterContext(property.Name);
+                VisitValue(property.Value);
+                ExitContext();
+            }
+        }
+
+        private void VisitValue(JsonElement value)
+        {
+            switch (value.ValueKind)
+            {
+                case JsonValueKind.Object:
+                    VisitElement(value);
+                    break;
+
+                case JsonValueKind.Array:
+                    var index = 0;
+                    foreach (var arrayElement in value.EnumerateArray())
+                    {
+                        EnterContext(index.ToString());
+                        VisitValue(arrayElement);
+                        ExitContext();
+                        index++;
+                    }
+
+                    break;
+
+                case JsonValueKind.Number:
+                case JsonValueKind.String:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Null:
+                    var key = _currentPath;
+                    if (_data.ContainsKey(key))
+                    {
+                        throw new FormatException($"Error KeyIsDuplicated {key}");
+                    }
+
+                    if (_keysToDecrypt.Contains(key))
+                    {
+                        _data[key] = _crypter.DecryptString(value.ToString());
+                    }
+                    else
+                    {
+                        _data[key] = value.ToString();
+                    }
+
+                    break;
+
+                case JsonValueKind.Undefined:
+                default:
+                    throw new FormatException($"Error UnsupportedJSONToken {value.ValueKind}");
+            }
+        }
+
+        private void EnterContext(string context)
+        {
+            _context.Push(context);
+            _currentPath = ConfigurationPath.Combine(_context.Reverse());
+        }
+
+        private void ExitContext()
+        {
+            _context.Pop();
+            _currentPath = ConfigurationPath.Combine(_context.Reverse());
         }
     }
 }
